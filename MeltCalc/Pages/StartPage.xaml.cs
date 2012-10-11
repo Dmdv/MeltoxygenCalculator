8db12212081e8f992a83eed3822315c4f7dd827f
Dmitry DA <dimos-d@yandex.ru>
2012-10-11 17:16:09 +0400

    Step1

--- MeltCalc/Pages/StartPage.xaml.cs
+++ MeltCalc/Pages/StartPage.xaml.cs
@@ -1,27 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MeltCalc.Pages
 {
 	/// <summary>
 	/// Interaction logic for StartPage.xaml
 	/// </summary>
-	public partial class StartPage : Page
+	public partial class StartPage
 	{
 		public StartPage()
 		{
 			InitializeComponent();
+		}
+
+		private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			if (NavigationService != null) 
+				NavigationService.Navigate(new Uri(@"Pages\Step1.xaml", UriKind.Relative));
 		}
 	}
 }
