3fe813adcdd899f0083d0208e497ee938437b2f4
Dmitry DA <dimos-d@yandex.ru>
2012-10-22 02:57:20 +0400

    Step13

---
+++ MeltCalc/Pages/Step12.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MeltCalc.Pages
+{
+	/// <summary>
+	/// Interaction logic for Step12.xaml
+	/// </summary>
+	public partial class Step12
+	{
+		public Step12()
+		{
+			InitializeComponent();
+		}
+
+		private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			if (NavigationService != null)
+				NavigationService.Navigate(new Uri(@"Pages\Step13.xaml", UriKind.Relative));
+		}
+
+		private void CommandBinding_CanPrevious(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void CommandBinding_PreviousPage(object sender, ExecutedRoutedEventArgs e)
+		{
+			if (NavigationService != null)
+				NavigationService.Navigate(new Uri(@"Pages\Step11.xaml", UriKind.Relative));
+		}
+	}
+}
---
+++ MeltCalc/Pages/Step13.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MeltCalc.Pages
+{
+	/// <summary>
+	/// Interaction logic for Step12.xaml
+	/// </summary>
+	public partial class Step13
+	{
+		public Step13()
+		{
+			InitializeComponent();
+		}
+
+		private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			if (NavigationService != null)
+				NavigationService.Navigate(new Uri(@"Pages\Step14.xaml", UriKind.Relative));
+		}
+
+		private void CommandBinding_CanPrevious(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void CommandBinding_PreviousPage(object sender, ExecutedRoutedEventArgs e)
+		{
+			if (NavigationService != null)
+				NavigationService.Navigate(new Uri(@"Pages\Step12.xaml", UriKind.Relative));
+		}
+	}
+}
