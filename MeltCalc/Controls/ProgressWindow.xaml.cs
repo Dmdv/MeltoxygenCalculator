5119f985b4d017d7bc9d683e2296a761dfb1f064
Dmitry DA <dimos-d@yandex.ru>
2013-02-22 01:47:14 +0400

    Added progress window

---
+++ MeltCalc/Controls/ProgressWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace MeltCalc.Controls
+{
+	/// <summary>
+	/// Interaction logic for ProgressWindow.xaml
+	/// </summary>
+	public partial class ProgressWindow
+	{
+		public ProgressWindow()
+		{
+			InitializeComponent();
+			Topmost = true;
+			ShowInTaskbar = false;
+		}
+	}
+}
