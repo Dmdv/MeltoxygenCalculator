3cedba90204bdc9c0782815f8d72355688713470
Dmitry DA <dimos-d@yandex.ru>
2013-05-23 09:18:03 +0400

    Saving results

--- MeltCalc/Pages/Results.xaml.cs
+++ MeltCalc/Pages/Results.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
 using MeltCalc.Chemistry;
 
 namespace MeltCalc.Pages
@@ -7,7 +9,7 @@ namespace MeltCalc.Pages
 	/// <summary>
 	/// Interaction logic for Results.xaml
 	/// </summary>
-	public partial class Results : Page
+	public partial class Results
 	{
 		public Results()
 		{
@@ -27,6 +29,93 @@ namespace MeltCalc.Pages
 
 			// TODO: Черный шрифт окон.
 			// TODO: Проверка на отрицательность
+
+			SaveResults();
+		}
+
+		private void SaveResults()
+		{
+			var text = new StringBuilder()
+
+				.Append(ResultsRow("C чугуна", Tube.Чугун.C))
+				.Append(ResultsRow("Si чугуна", Tube.Чугун.Si))
+				.Append(ResultsRow("Mn чугуна", Tube.Чугун.Mn))
+				.Append(ResultsRow("P чугуна", Tube.Чугун.P))
+				.Append(ResultsRow("S чугуна", Tube.Чугун.S))
+
+				.Append(ResultsRow("C лома", Tube.Лом.C))
+				.Append(ResultsRow("Si лома", Tube.Лом.Si))
+				.Append(ResultsRow("Mn лома", Tube.Лом.Mn))
+				.Append(ResultsRow("P лома", Tube.Лом.P))
+				.Append(ResultsRow("S лома", Tube.Лом.S))
+
+				.Append(ResultsRow("Масса чугуна", Estimation.GchugSAVE[Estimation.GchugSAVE.Length - 1]))
+				.Append(ResultsRow("Масса лома", Estimation.GlomSAVE[Estimation.GlomSAVE.Length - 1]))
+				.Append(ResultsRow("Масса ост. шлака", Tube.ОставленныйШлак.G))
+				.Append(ResultsRow("Масса микс. шлака", Tube.МиксерныйШлак.G))
+				.Append(ResultsRow("Масса шлака", Estimation.GshlSAVE[Estimation.GshlSAVE.Length - 1]))
+
+				.Append(ResultsRow("Выход стали", Tube.Сталь.GYield))
+				.Append(ResultsRow("C стали", Tube.Сталь.C))
+				.Append(ResultsRow("Si стали", Tube.Сталь.Si))
+				.Append(ResultsRow("Mn стали", Tube.Сталь.Mn))
+				.Append(ResultsRow("P стали", Tube.Сталь.P))
+				.Append(ResultsRow("S стали", Tube.Сталь.S))
+
+				.Append(ResultsRow("FeO шлака всего", Tube.Шлак.TOTALFeO))
+				.Append(ResultsRow("Mn шлака", Estimation.MnOshlSAVE[Estimation.MnOshlSAVE.Length - 1]))
+				.Append(ResultsRow("P2P5 шлака", Tube.Шлак.P2O5))
+
+				.Append(ResultsRow("Основность", Tube.Шлак.B))
+
+				.Append(ResultsRow("Известь", Estimation.GizvSAVE[Estimation.GizvSAVE.Length - 1]))
+				.Append(ResultsRow("Известняк", Estimation.GizkSAVE[Estimation.GizkSAVE.Length - 1]))
+				.Append(ResultsRow("Доломит", Estimation.GdolSAVE[Estimation.GdolSAVE.Length - 1]))
+				.Append(ResultsRow("Вл. Доломит", Estimation.GvldolSAVE[Estimation.GvldolSAVE.Length - 1]))
+				.Append(ResultsRow("ИМФ", Estimation.GimfSAVE[Estimation.GimfSAVE.Length - 1]))
+
+				.Append(ResultsRow("Песок", Tube.Песок.G))
+				.Append(ResultsRow("Кокс", Tube.Кокс.G))
+				.Append(ResultsRow("Окатыши", Tube.Окатыши.G))
+				.Append(ResultsRow("Руда", Tube.Руда.G))
+				.Append(ResultsRow("Окалина", Tube.Окалина.G))
+				.Append(ResultsRow("Агломерат", Tube.Агломерат.G))
+
+				.Append(ResultsRow("Шпат", Estimation.GshpSAVE[Estimation.GshpSAVE.Length - 1]))
+				.Append(ResultsRow("Объём О2", Estimation.VdutSAVE[Estimation.VdutSAVE.Length - 1]))
+				.Append(ResultsRow("Темп воздуха", Params.AirTemp))
+				.Append(ResultsRow("Темп чугуна", Tube.Чугун.T))
+				.Append(ResultsRow("Темп стали", Tube.Сталь.T))
+
+				.Append(ResultsRow("Ar дно, м3/мин", Params.BlowingTime == 0 ? 0 : AdaptationData.VArBlow / Params.BlowingTime))
+
+				.Append(ResultsRow("Футеровка", Tube.Футеровка.G))
+				.Append(ResultsRow("О2", Tube.Дутье.O2))
+				.Append(ResultsRow("Ar", Tube.Дутье.Ar))
+				.Append(ResultsRow("N2", Tube.Дутье.N2))
+				.Append(ResultsRow("Доля легковеса", Tube.Лом.DolyaLegkovesa))
+				.Append(ResultsRow("Пакеты", Tube.Пакеты.G))
+
+				// Пропущены COMPAIR, FutTypeSelected, SelectedPlant
+
+			.ToString();
+
+			// TODO: В адаптации поправить хардкоженный параметр
+
+			try
+			{
+				File.WriteAllText("Results.txt", text);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Ошибка при записи результатов:\r\n" + ex.Message, "Ошибка записи",
+				                MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+
+		private string ResultsRow(string parameter, double value)
+		{
+			return string.Format("{0}:\t\t\t{1:0.000}{2}", parameter, value, Environment.NewLine);
 		}
 
 		private void LoadCommonParams()
